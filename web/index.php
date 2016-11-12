5115702dd46c745f13344231075bfaf12e3caa77
Sunday Ayandokun <sunday.ayandokun@gmail.com>
2016-11-12 23:53:23 +0200

    Initial project setup

---
+++ web/index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by: Sunday Ayandokun
+ * Email: sunday.ayandokun@gmail.com
+ * Date: 12/11/2016
+ * Time: 6:21 PM
+ */
+
+require '../vendor/autoload.php';
+
+
+$app = \Lustre\Application::singleton();
+
+/*
+|-------------------------------------------------------------------
+| Register Application Routes
+|-------------------------------------------------------------------
+*/
+require __DIR__ . '/../application/routes.php';
+
+/*
+|-------------------------------------------------------------------
+| Running the Application
+|-------------------------------------------------------------------
+*/
+$app->run();
