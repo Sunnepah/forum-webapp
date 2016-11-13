c570964a80f218db8d147e1bea1ff234dc6f3e87
Sunday Ayandokun <sunday.ayandokun@gmail.com>
2016-11-13 19:38:44 +0200

    Added validation to image uploader

--- application/Libraries/ImageUploader.php
+++ application/Libraries/ImageUploader.php
@@ -14,11 +14,14 @@ use Application\Config\ServiceConfig;
 class ImageUploader
 {
     public static function upload_image($image_data) {
+        if (!self::isImageSupported($image_data))
+            return false;
+
         $s3Client = new S3Client([
-            'version'     => 'latest',
-            'region'      => 'eu-central-1',
+            'version' => 'latest',
+            'region' => 'eu-central-1',
             'credentials' => [
-                'key'    => ServiceConfig::$services['s3']['AccessKey_ID'],
+                'key' => ServiceConfig::$services['s3']['AccessKey_ID'],
                 'secret' => ServiceConfig::$services['s3']['SecretAccess_Key'],
             ],
         ]);
@@ -37,5 +40,31 @@ class ImageUploader
         }
 
         return false;
+    }
+
+    public static function isImageSupported($image_data) {
+        /** TODO: Move to App config file */
+        $maxFileSize = 2097152;
+        $maxWidth = 1920;
+        $maxHeight = 1080;
+
+        $path_info = pathinfo($image_data["userfile"]["name"]);
+        if (!self::isValidType($path_info['extension']))
+            return false;
+
+        if ($image_data['userfile']['size'] > $maxFileSize)
+            return false;
+
+        $image_dimensions = getimagesize($image_data['userfile']['tmp_name']);
+        $image_width = $image_dimensions[0];
+        $image_height = $image_dimensions[1];
+        if (($image_width > $maxWidth) || ($image_height > $maxHeight))
+            return false;
+
+        return true;
+    }
+
+    private static function isValidType($image_type) {
+       return in_array($image_type, ['jpg', 'jpeg', 'png', 'gif']);
     }
 }
